694f961c441ec0690b2155f40150874a45118fcd
Tobias <33400774+Dova81@users.noreply.github.com>
2019-05-03 20:24:57 -0300

    Limpio imports innecesarios de App

--- C4G.Grupo7.Frontend/chicas-en-tecnologia/src/App.js
+++ C4G.Grupo7.Frontend/chicas-en-tecnologia/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Inicio from './Components/Inicio'
 
 function App() {
   return (
     <div >
-      
+      <Inicio/>
     </div>
   );
 }
