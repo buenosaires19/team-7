d2df4b052a4a8886c28fe0bf9407c978c45451f0
fedes112 <fedes112@users.noreply.github.com>
2019-05-03 19:22:28 -0300

    Initial commit

---
+++ C4G.Grupo7.Backend/src/main/java/grupo7Backend/C4G/Grupo7/Application.java
@@ -0,0 +1,13 @@
+package grupo7Backend.C4G.Grupo7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
