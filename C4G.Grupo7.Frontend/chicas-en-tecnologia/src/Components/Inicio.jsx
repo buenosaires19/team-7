11863fb9aa4ab3bfd9aeb61ce47ae50c7bb2c982
Tobias <33400774+Dova81@users.noreply.github.com>
2019-05-03 20:25:24 -0300

    Creo Componentes inicio
    Creo componente inicio y su Css acorde

--- C4G.Grupo7.Frontend/chicas-en-tecnologia/src/Components/Inicio.jsx
+++ C4G.Grupo7.Frontend/chicas-en-tecnologia/src/Components/Inicio.jsx
@@ -1,11 +1,78 @@
-import React, { Component } from 'react'
+import React from 'react';
+//import API from '../../ApiComponent'
+import './style.css'
 
-export default class Inicio extends Component {
-  render() {
-    return (
-      <div>
+
+class ComponenteABM extends React.Component {
+
+    constructor() {
+        super();
+
+    }
+
+
+
+    renderUsuarios() {
+
+        return(
+            <div class="card">
+                {this.state.usuarios.map(usuario =>
+                    <div class="card-body" onClick={() => 
+                    this.setState({usuarioSeleccionado: usuario})}
+                    style={{
+                        backgroundColor: this.state.usuarioSeleccionado.name 
+                        && this.state.usuarioSeleccionado.name === usuario.name ? 'red' : 'beige',
+                        fontSize: '30px',
+                    }}
+                    >
+                    {usuario.name}
+                    
+                    </div>
+                )}
+            </div>
+            );
+    }
+
+    goPostulante() {
+       this.props.history.push('/postulante');
+    }
+
+   /* goUser() {
+        this.props.history.push(`/user/${this.state.usuarioSeleccionado.id}`);
+    }
+
+    deleteUser() {
+        API.delete(`/user/${this.state.usuarioSeleccionado.id}/delete`)
+        .then(console.log(this.state)).catch(console.log("No pillo"))
+    }
+    */
+    renderButtons() {
+        return(
+        <div class="Button-flex">
+     
+            <button type="button" className="btn btn-primary" onClick={() =>this.goPostulante()}> Postular </button> 
         
-      </div>
-    )
-  }
+            <button type="button" className="btn btn-primary" > Listado </button>
+    
+        </div>
+        );
+    }
+
+    render() {
+
+
+        return (
+            <body>
+                <div>   
+                    
+                    {this.renderButtons()}    
+                </div>
+            </body>
+        );
+    }
+
+
+
 }
+
+export default ComponenteABM;
