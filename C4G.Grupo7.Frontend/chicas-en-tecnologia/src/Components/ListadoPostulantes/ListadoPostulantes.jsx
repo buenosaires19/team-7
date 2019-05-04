38d60aaf1c1886e89edff13897c2ddfbc343e71a
Tobias <33400774+Dova81@users.noreply.github.com>
2019-05-04 10:11:13 -0300

    Fix

--- C4G.Grupo7.Frontend/chicas-en-tecnologia/src/Components/ListadoPostulantes/ListadoPostulantes.jsx
+++ C4G.Grupo7.Frontend/chicas-en-tecnologia/src/Components/ListadoPostulantes/ListadoPostulantes.jsx
@@ -10,12 +10,17 @@ class ListadoPostulantes extends React.Component {
         super();
         this.state = {
             postulantes: [],
+            postulantesFiltrados:[],
+            provincia:'',
+            oficios:'',
         } 
 
     }
 
     componentDidMount(){
-        API.get("/postulantes").then(postulantes1 => this.setState({postulantes: postulantes1})).catch(console.log());
+        
+     
+        API.get("/postulantes/").then(postulantes1 => this.setState({postulantes: postulantes1,postulantesFiltrados:postulantes1})).catch(console.log());
     }
 
 
@@ -31,7 +36,7 @@ class ListadoPostulantes extends React.Component {
     
 
     renderPostulantes() {
-       return this.state.postulantes.map(postulante =>  <div class="card mb-3 card-custom">
+       return this.state.postulantesFiltrados.map(postulante =>  <div class="card mb-3 card-custom">
         <div class="row no-gutters">
             <div class="col-md-4">
                 <img src={chica} class="rounded-circle center" alt="..."/>
@@ -39,26 +44,60 @@ class ListadoPostulantes extends React.Component {
             <div class="col-md-8">
                 <div class="card-body">
                     <h5 class="card-title">{postulante.nombre} {postulante.apellido}</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <button onClick={() => this.redirect(postulante.id)}><p class="card-text"><small class="text-muted"></small></p></button>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>                    
+                    <div>
+                        <button class="btn btn-lg btn-primary" onClick={() => this.redirect(postulante.id)} >Ver</button>  
+                    </div>
                 </div>
             </div>
         </div>
     </div>)
     }
 
+    irInicio() {
+        this.props.history.push("/");
+    }
+
+    setProvincia(elem){
+        console.log(elem.target.value);
+        
+        this.setState({
+            provincia: elem.target.value
+        });
+
+        const x =elem.target.value;
+        const filtrados =  this.state.postulantes.filter(p => p.localidad.provincia === x);
+
+        console.log(filtrados);
+        
+        
+        this.setState({postulantesFiltrados:filtrados})    
+    }
+
+    renderMenu() {
+        return(
+        <nav class="navbar navbar-dark bg-dark">
+            <div class="navbar-brand" onClick={() => this.irInicio()}>Inicio</div>
+            <a href="https://www.chicasentecnologia.org/"><span>FAQ</span></a>
+        </nav>
+        );
+    }
+
     render() {
 
 
         return (
-            <body >
-                <h1>Postuladas</h1>
-                <div/><br/>
-                <select class="form-control col-sm-4" id="selectProvinicias"  onChange={event => this.setLocalidad(event)}>{this.provincias()}</select>
-                <div className="container backScroll full-screen">   
-                    {this.renderPostulantes()}    
-                </div>
-            </body>
+            <main>
+                {this.renderMenu()}
+                <body >
+                    <h1>Postuladas</h1>
+                    <div/><br/>
+                    <select class="form-control col-sm-4" id="selectProvinicias"  onChange={event => this.setProvincia(event)}>{this.provincias()}</select>
+                    <div className="container backScroll full-screen">   
+                        {this.renderPostulantes()}    
+                    </div>
+                </body>
+            </main>
         );
     }
 
