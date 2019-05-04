fa91e223c7845777259c7493019b08e321a01a5d
Tobias <33400774+Dova81@users.noreply.github.com>
2019-05-04 00:07:22 -0300

    Fade en el style

--- C4G.Grupo7.Frontend/chicas-en-tecnologia/src/Components/Inicio.jsx
+++ C4G.Grupo7.Frontend/chicas-en-tecnologia/src/Components/Inicio.jsx
@@ -3,12 +3,27 @@ import React from 'react';
 import './style.css'
 import logo from '../logo-big.svg'
 import chica from '../Components/Pictures/chica.jpg'
+import API from '../API'
+
 
 class inicio extends React.Component {
 
     constructor() {
         super();
+        this.state = {
+            usuarioSeleccionado: {
+                "id": 0,
+                "name": "",
+                "surname": "",
+                "email": "",
+                "dni": 0
+            }
+        } 
 
+    }
+
+    componentDidMount(){
+        API.get("/postulante/masvisitado").then(users => this.setState({usuarios: users})).catch(console.log("Fail"));
     }
 
     renderUsuarios() {
@@ -52,19 +67,19 @@ class inicio extends React.Component {
                     <div className="row">
                         <img src={logo}  alt="chica en tecnologia"/> 
                     </div>
-                    <div className="row">
+                    <div className="row ">
                     
-                    <div className="container text-center"> 
-                    <img src={chica} class="rounded" alt="chica test"/> <h1>Maria elena</h1>
+                    <div id='fade' className="container text-center"> 
+                    <img src={chica} class="rounded achicarImg" alt="chica test"/> <h1>Maria elena</h1><h2>Psicologa</h2>
                         
-                        </div>
+                    </div>
                     </div>
                     <div className="row text-center">
                         <div className="col-md-6">
-                            <button type="button" className="btn btn-primary" onClick={() => this.goPostulante()}> Postular </button>
+                            <button type="button" className="btn btn-outline-light" onClick={() => this.goPostulante()}> Postular </button>
                         </div>
                         <div className="col-md-6">
-                            <button type="button" className="btn btn-primary" > Listado </button>
+                        <button type="button" class="btn btn-outline-light">Listas</button>
                         </div>
                     </div>
                 </div>
