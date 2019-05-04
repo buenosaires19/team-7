4c246a70cec4591f35e9ebc1f09f61982f234944
Elias Emanuel Copa <eliasemanuelcopa@outlook.com>
2019-05-03 21:54:06 -0300

    feat: Added logo and background

--- C4G.Grupo7.Frontend/chicas-en-tecnologia/src/Components/Inicio.jsx
+++ C4G.Grupo7.Frontend/chicas-en-tecnologia/src/Components/Inicio.jsx
@@ -1,78 +1,82 @@
 import React from 'react';
 //import API from '../../ApiComponent'
 import './style.css'
+import logo from '../logo-big.svg'
 
 
-class ComponenteABM extends React.Component {
+class inicio extends React.Component {
 
     constructor() {
         super();
 
     }
 
-
-
     renderUsuarios() {
 
-        return(
+        return (
             <div class="card">
                 {this.state.usuarios.map(usuario =>
-                    <div class="card-body" onClick={() => 
-                    this.setState({usuarioSeleccionado: usuario})}
-                    style={{
-                        backgroundColor: this.state.usuarioSeleccionado.name 
-                        && this.state.usuarioSeleccionado.name === usuario.name ? 'red' : 'beige',
-                        fontSize: '30px',
-                    }}
+                    <div class="card-body" onClick={() =>
+                        this.setState({ usuarioSeleccionado: usuario })}
+                        style={{
+                            backgroundColor: this.state.usuarioSeleccionado.name
+                                && this.state.usuarioSeleccionado.name === usuario.name ? 'red' : 'beige',
+                            fontSize: '30px',
+                        }}
                     >
-                    {usuario.name}
-                    
+                        {usuario.name}
+
                     </div>
                 )}
             </div>
-            );
+        );
     }
 
     goPostulante() {
-       this.props.history.push('/postulante');
+        this.props.history.push('/postulante');
     }
 
-   /* goUser() {
-        this.props.history.push(`/user/${this.state.usuarioSeleccionado.id}`);
-    }
-
-    deleteUser() {
-        API.delete(`/user/${this.state.usuarioSeleccionado.id}/delete`)
-        .then(console.log(this.state)).catch(console.log("No pillo"))
-    }
-    */
+    /* goUser() {
+         this.props.history.push(`/user/${this.state.usuarioSeleccionado.id}`);
+     }
+ 
+     deleteUser() {
+         API.delete(`/user/${this.state.usuarioSeleccionado.id}/delete`)
+         .then(console.log(this.state)).catch(console.log("No pillo"))
+     }
+     */
     renderButtons() {
-        return(
-        <div class="Button-flex">
-     
-            <button type="button" className="btn btn-primary" onClick={() =>this.goPostulante()}> Postular </button> 
-        
-            <button type="button" className="btn btn-primary" > Listado </button>
-    
-        </div>
+        return (
+            <div id="full-screen" className="container-fluid">
+                <div className="container">
+                    <div className="row">
+                        <img src={logo}  alt="chica en tecnologia"/> 
+                    </div>
+                    <div className="row">
+                        
+                    </div>
+                    <div className="row text-center">
+                        <div className="col-md-6">
+                            <button type="button" className="btn btn-primary" onClick={() => this.goPostulante()}> Postular </button>
+                        </div>
+                        <div className="col-md-6">
+                            <button type="button" className="btn btn-primary" > Listado </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 
     render() {
-
-
         return (
             <body>
-                <div>   
-                    
-                    {this.renderButtons()}    
+                <div>
+                    {this.renderButtons()}
                 </div>
             </body>
         );
     }
-
-
-
 }
 
-export default ComponenteABM;
+export default inicio;
