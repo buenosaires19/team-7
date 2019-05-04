5086c571afb9d99f51312de6ff368e5f63385d46
Elias Emanuel Copa <eliasemanuelcopa@outlook.com>
2019-05-04 04:32:14 -0300

    feat: fixed

--- C4G.Grupo7.Frontend/chicas-en-tecnologia/src/Components/Inicio.jsx
+++ C4G.Grupo7.Frontend/chicas-en-tecnologia/src/Components/Inicio.jsx
@@ -11,24 +11,24 @@ class inicio extends React.Component {
     constructor() {
         super();
         this.state = {
-                nombre: "",
-                apellido: "",
-                fechaNacimiento: "",
-                descripcion: "",
-                localidad: {
-                    pais: "",
-                    provincia: "",
-                    localidad: "",  
-                },
-                contenido: "",
-                foto: "",
-                areaEspecializacion: "",
-        } 
+            nombre: "",
+            apellido: "",
+            fechaNacimiento: "",
+            descripcion: "",
+            localidad: {
+                pais: "",
+                provincia: "",
+                localidad: "",
+            },
+            contenido: "",
+            foto: "",
+            areaEspecializacion: "",
+        }
 
     }
 
-    componentDidMount(){
-        API.get("/postulante/masvisitado").then(users => this.setState({usuarios: users})).catch(console.log("Fail"));
+    componentDidMount() {
+        API.get("/postulante/masvisitado").then(users => this.setState({ usuarios: users })).catch(console.log("Fail"));
     }
 
 
@@ -36,30 +36,45 @@ class inicio extends React.Component {
         this.props.history.push('/postulante');
     }
 
-     goListado() {
-         this.props.history.push(`/postulados`);
-     }
-     /*
-     deleteUser() {
-         API.delete(`/user/${this.state.usuarioSeleccionado.id}/delete`)
-         .then(console.log(this.state)).catch(console.log("No pillo"))
-     }
-     */
+    goListado() {
+        this.props.history.push(`/postulados`);
+    }
+    /*
+    deleteUser() {
+        API.delete(`/user/${this.state.usuarioSeleccionado.id}/delete`)
+        .then(console.log(this.state)).catch(console.log("No pillo"))
+    }
+    */
 
-    renderCard(){
-        return(
-                <div className="container borderShadow">
-                    <div className='row'>
-                        <div className='col-md-6'>
-                        <img class="imgRedondeada" src={chica} alt="Test"/>
-                        </div>
-                        <div className="col-md-6 padding6">
-                            <h5 class="card-title">Alejandra Rios</h5>
-                            <p class="card-text">"La vida es una y hay que vivirla. La biotecnologia me cambio la vida."</p>
-                            <p class="card-text">Bioquimica reconocida mundialmente</p>
+    renderCard() {
+        return (
+            // <div className="container borderShadow">
+            //     <div className='row'>
+            //         <div className='col-md-6'>
+            //         <img class="imgRedondeada" src={chica} alt="Test"/>
+            //         </div>
+            //         <div className="col-md-6 padding6">
+            //             <h5 class="card-title">Alejandra Rios</h5>
+            //             <p class="card-text">"La vida es una y hay que vivirla. La biotecnologia me cambio la vida."</p>
+            //             <p class="card-text">Bioquimica reconocida mundialmente</p>
+            //         </div>
+            //     </div>
+            // </div>
+            <div class="card mb-3 card-custom">
+                <div class="row no-gutters">
+                    <div class="col-md-4">
+                        <img src={chica} class="rounded-circle" alt="..."/>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                         </div>
                     </div>
                 </div>
+            </div>
+
         )
     }
     renderButtons() {
@@ -68,23 +83,23 @@ class inicio extends React.Component {
                 <div className="container">
 
 
-                
-                    <div className="row">
-                        <img src={logo}  alt="chica en tecnologia"/> 
-                    </div>
-                    <div className="row">
-                    <div id='fade' className="container text-center"> 
 
-                    {this.renderCard()}
-                        
+                    <div className="row">
+                        <img src={logo} alt="chica en tecnologia" />
                     </div>
+                    <div className="row">
+                        <div id='fade' className="container text-center">
+
+                            {this.renderCard()}
+
+                        </div>
                     </div>
                     <div className="row text-center">
                         <div className="col-md-6">
                             <button type="button" className="btn btn-outline-light" onClick={() => this.goPostulante()}> Postular </button>
                         </div>
                         <div className="col-md-6">
-                        <button type="button" class="btn btn-outline-light" onClick={() => this.goListado()}>Listas</button>
+                            <button type="button" class="btn btn-outline-light" onClick={() => this.goListado()}>Listas</button>
                         </div>
                     </div>
                 </div>
